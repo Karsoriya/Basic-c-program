a22db131507ce0a6e3a37c345cda1e0202cfdfc9
Karsoriya <soriyabasharat@gmail.com>
2024-09-25 19:54:16 +0530

    Update 05.c

--- 05.c
+++ 05.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #define PI 3.1415925359
 int main(){
-    double a,r;
+    double area,r;
     printf("Enter the radius of circle: \n");
     scanf("%lf",&r);
-    a = PI*r*r;
+    area = PI*r*r;
     printf("Area of circle is %lf",a);
 
     return 0;
