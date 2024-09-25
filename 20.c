0b6a74b59b13a7c3ae99bd522b0f7b7a3fc92a7a
Karsoriya <soriyabasharat@gmail.com>
2024-09-25 20:25:55 +0530

    Update 20.c

--- 20.c
+++ 20.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() 
 {
-  int n,i,x=1;
+  int n,i,flag=1;
   printf("Enter  any number: ");
   scanf("%d",&n);
   for(i=2 ; i<= n-1; i++)
@@ -9,12 +9,12 @@ int main()
       if(n%i==0)
       { 
         printf("%d is not a prime no.",n);
-        x=0;
+        flag=0;
         break;
       }  
       
     }
-  if(x==1)      
+  if(flag==1)      
   printf("%d is a prime no.",n);
         
       
